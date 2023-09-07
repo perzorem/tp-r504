aa5ebb5b43cf2f51a50f268ca399a533bebed4c8
perzorem <masteremipp@gmail.com>
2023-09-07 14:51:36 +0200

    Modification pour fonctionner avec fonctions.py

--- prog1.py
+++ prog1.py
@@ -1,6 +1,10 @@
+import fonctions as f
+### use of the fonctions.py file
+
 condition = True
 while condition == True:
 	print("Hello, World!")
-	x = int(input("entrez à nombre : "))
-	x = x**2
-	print(x)
+	a = int(input("entrez le nombre a : "))
+	b = int(input("entrez le nombre b : "))
+	res = f.puissance(a,b)
+	print(res)
