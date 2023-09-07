b7a82179a03ace546724af384ee903a4c665b21e
perzorem <masteremipp@gmail.com>
2023-09-07 14:38:33 +0200

    ajout de lignes pour Q2.2

--- prog1.py
+++ prog1.py
@@ -1,4 +1,6 @@
-print("Hello, World!")
-x = int(input("entrez à nombre : "))
-x = x**2
-print(x)
+condition = True
+while condition == True:
+	print("Hello, World!")
+	x = int(input("entrez à nombre : "))
+	x = x**2
+	print(x)
