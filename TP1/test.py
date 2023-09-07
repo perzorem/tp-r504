f7195f8d01ca0e7e365197ca50556ce974205389
perzorem <masteremipp@gmail.com>
2023-09-07 16:51:44 +0200

    modification du programme pour la Q.5

--- TP1/test.py
+++ TP1/test.py
@@ -10,7 +10,7 @@ def test_2():
 	assert f.puissance(-2,2) == 4
 
 def test_3():
-	assert f.puissance(0,-3) == 0
+	assert f.puissance(0,-3) == vous ne pouvez pas avoir une puissance négative
 	assert f.puissance(0,3) == 0
 	assert f.puissance(0,0) == 1
 	assert f.puissance(-2,0) == -1
