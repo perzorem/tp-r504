947522bbea57326e9daba7b67dcc416bb397a29c
perzorem <masteremipp@gmail.com>
2023-09-07 16:47:00 +0200

    modification du programme pour la Q.5

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -2,5 +2,17 @@ def puissance(a,b):
 	if type(a)is not int or type(b)is not int:
 		raise TypeError("Only integers are allowed")
 	else:
-		x = a**b
-	return x
+		result = a
+		if a == 0 and b != 0:
+			return(a)
+		if a == 0 and b == 0:
+			return(1)
+		if a != 0 and b ==0:
+			if a<0:
+				return(-1)
+			else:
+				return(1)
+		if a != 0 and b != 0:
+			for i in range(1,b,1):
+				result = result*a
+			return(result)
--- TP1/test.py
+++ TP1/test.py
@@ -9,3 +9,9 @@ def test_2():
 	assert f.puissance(-2,3) == -8
 	assert f.puissance(-2,2) == 4
 
+def test_3():
+	assert f.puissance(0,-3) == 0
+	assert f.puissance(0,3) == 0
+	assert f.puissance(0,0) == 1
+	assert f.puissance(-2,0) == -1
+	assert f.puissance(2,0) == 1
