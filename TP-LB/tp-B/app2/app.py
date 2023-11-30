c5f4f6abfc4f54b7f6e5686ce3c2254e1f2539f2
Perzo-Piel <aaaa@bbb.fr>
2023-11-30 15:18:35 +0100

    toto

--- TP-LB/tp-B/app2/app.py
+++ TP-LB/tp-B/app2/app.py
@@ -4,7 +4,7 @@ import json
 app = Flask(__name__)
 @app.route(’/’)
 def hello_world():
-	return ’serveur 1’ (’serveur 2’ pour celui dans app2/)
+	return ’serveur 2’
 
 if __name__ == ’__main__’:
 	app.run(debug=True, host=’0.0.0.0’)
