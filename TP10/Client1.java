313ee8c8b26c605c50a9adfeb2c35884c7010d7b
perzorem <remi.perzopiel.pro@gmail.com>
2024-02-02 05:20:26 -0500

    modif

--- TP10/Client1.java
+++ TP10/Client1.java
@@ -1,15 +1,38 @@
 import java.io.*;
-import java.net.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
 
-public class Clienthttp
+public class Client1
 {
 	public static void main(String[] args) 
 	{	
 		try
 		{
+			if (args.length == 0) 
+			{
+            	System.out.println("at least one arguments required");
+ 				System.exit(1);       	
+			} 
+           	System.out.println("Arguments number : " + args.length);
+           	System.out.println("Given Arguments :");
+           
+           	for (int i = 0; i < args.length; i++) 
+			{
+               	System.out.println("Argument " + (i + 1) + ": " + args[i]);
+			}
+			CloseableHttpClient client = HttpClients.createDefault();
+			String url = " http ://" + args[0];
+			HttpGet request = new HttpGet (url);
 
-			
+			System . out . println ( " Executing request " + request.getRequestLine ( ) ) ;
+			CloseableHttpResponse resp = client . execute ( request ) ;
 
+			System . out . println ( "Response Line : " + resp . getStatusLine ( ) ) ;
+			System . out . println ( "Response Code: " + resp . getStatusLine ( ) . getStatusCode ( ) ); 
+
+            
 		}
 		catch (IOException e)
 		{
